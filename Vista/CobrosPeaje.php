4444b6a362dd0322685152c70970243060b78e91
Maria <mabreca15@gmail.com>
2024-06-10 17:13:52 -0600

    estilos cobros

--- Vista/CobrosPeaje.php
+++ Vista/CobrosPeaje.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cobros -- PassWize</title>
     <link rel="icon" type="image/png" href="../img/icono.png">
+    <link rel="stylesheet" href="Estilos/CobrosPeaje.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
