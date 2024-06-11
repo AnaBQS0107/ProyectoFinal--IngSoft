30a8e1fb4a304cc0e5e846c8a7047171196651e2
Ana <anabqs0107@gmail.com>
2024-06-10 19:44:34 -0600

    Ajustes de estilos filtros

--- Vista/Filtros.php
+++ Vista/Filtros.php
@@ -11,6 +11,7 @@ require_once '../Controlador/Cobro.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Cobros</title>
     <link rel="icon" type="image/png" href="../img/icono.png">
+    <link rel="stylesheet" href="Estilos/Filtros.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -18,7 +19,7 @@ require_once '../Controlador/Cobro.php';
     <header>
         <?php include 'header.php'; ?>
     </header>
-    <br><br>
+    <br>
     <div class="container mt-5">
         <center><h1>Lista de Cobros</h1></center>
         <table class="table table-bordered table-dark">
