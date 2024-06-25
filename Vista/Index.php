d9c4f6703f4c2d507cb480ad192879332f3e353e
Ana <anabqs0107@gmail.com>
2024-06-25 17:47:39 -0600

    Arreglé el login (index.php)

--- Vista/Index.php
+++ Vista/Index.php
@@ -1,25 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../img/icono.png">
     <link rel="stylesheet" href="Estilos/login.css">
     <title>Login</title>
+
 </head>
+
 <body>
 
-<img src="../img/Login.svg" alt="Peaje" class="background">
+    <img src="../img/Login.svg" alt="Peaje" class="background">
 
-<form method="post" action="../Controlador/Login.php?action=login">
+    <div class="container-custom">
+        <form method="post" action="../Controlador/Login.php?action=login">
+            <center>
+                <h1>PassWize</h1>
+            </center>
 
-    <label for="Persona_Cedula" class="label_login">Cédula:</label>
-    <input type="text" id="Persona_Cedula" name="Persona_Cedula" placeholder="306220410" class="input_login" required>
+            <label for="Persona_Cedula" class="label_login">Cédula:</label>
+            <br><br>
+            <input type="text" id="Persona_Cedula" name="Persona_Cedula" placeholder="306220410" class="input_login" required>
 
-    <label for="contrasena" class="label_login">Contraseña:</label>
-    <input type="password" id="contrasena" name="contrasena" placeholder="********" class="input_login" required>
+            <label for="contrasena" class="label_login">Contraseña:</label>
+            <br><br>
+            <input type="password" id="contrasena" name="contrasena" placeholder="********" class="input_login"
+                required>
 
-    <button type="submit" class="button_login">Iniciar Sesión</button>
-</form>
+            <br> <br> 
+            <center> <button type="submit" class="button_login">Iniciar Sesión</button></center>
+
+        </form>
+    </div>
+
 </body>
+
 </html>
