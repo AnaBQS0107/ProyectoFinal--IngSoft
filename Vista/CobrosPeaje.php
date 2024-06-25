a163c183ee2c5ceeb9891d40ed8f204bfd66c155
Ana <anabqs0107@gmail.com>
2024-06-24 21:54:13 -0600

    No se

--- Vista/CobrosPeaje.php
+++ Vista/CobrosPeaje.php
@@ -116,7 +116,8 @@
         }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <?php include 'Footer.php'; ?>
+   
 </body>
-
+<br><br>
+<?php include 'Footer.php'; ?>
 </html>
