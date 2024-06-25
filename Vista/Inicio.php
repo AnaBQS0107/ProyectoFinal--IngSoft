6f93278260a10dbe73dc2a9d9ad52eb925c13b35
Maria <mabreca15@gmail.com>
2024-06-24 18:00:53 -0600

    footer

--- Vista/Inicio.php
+++ Vista/Inicio.php
@@ -31,6 +31,7 @@
     </section>
   </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <?php include 'Footer.php'; ?>
 </body>
 
 </html>
