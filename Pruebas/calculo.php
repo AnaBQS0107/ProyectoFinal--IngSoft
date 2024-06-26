f7db5e6e62aeb36d5c251979cbf69e17c4033d2c
EmmanuelCerdasB <emmanuelcb24@gmail.com>
2024-06-26 16:52:39 -0600

    prueba

---
+++ Pruebas/calculo.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $salario = $_POST['salario'];
+    $horas = $_POST['horas'];
+    $pago_extra = $salario * 1.5 * $horas;
+    header("Location: index.html?resultado=$pago_extra");
+    exit();
+}
+?>
