b0829544bdafac3ebaea170ba54e568e244f4c73
EmmanuelCerdasB <emmanuelcb24@gmail.com>
2024-06-26 20:41:02 -0600

    Arreglo footer

--- Vista/Footer.php
+++ Vista/Footer.php
@@ -1,26 +1,20 @@
-<footer class="text-light py-4">
+<footer class="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="Estilos/Footer.css">
-    <div class="footer-icon-container">
-    </div>
     <div class="container">
         <div class="footer-content">
-            <div class="about-us">
+            <div class="column">
                 <h3>Sobre nosotros</h3>
-                <br>
                 <p>PassWize es un servicio de gestión de peajes diseñado para facilitar tu viaje.</p>
             </div>
-            <div class="contact">
+            <div class="column">
                 <h3>Contacto</h3>
-                <br>
                 <p><i class="fas fa-phone"></i> 123456789</p>
                 <p><i class="fas fa-envelope"></i>Email: soporte@passwize.com</p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <center><p>&copy; <?php echo date('Y'); ?> PassWize. Todos los derechos reservados.</p></center>
-            </div>
+        <div class="footer-bottom text-center">
+            <p>&copy; <?php echo date('Y'); ?> PassWize. Todos los derechos reservados.</p>
         </div>
     </div>
 </footer>
