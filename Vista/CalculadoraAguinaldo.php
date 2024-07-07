d13cec3f49ad76ecd26b4178fd814b5bd248fbb4
Ana <anabqs0107@gmail.com>
2024-07-07 01:14:15 -0600

    avance diseño aguianaldo (Valeria)

--- Vista/CalculadoraAguinaldo.php
+++ Vista/CalculadoraAguinaldo.php
@@ -11,7 +11,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de Aguinaldo</title>
-    <link rel="stylesheet" href="Estilos/HorasExtras.css">
+    <link rel="stylesheet" href="Estilos/Aguinaldo.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script>
@@ -76,103 +76,103 @@ if (session_status() == PHP_SESSION_NONE) {
     </header>
 
     <div class="MainContentPlaceDiv">
-        <br>
-        <h1>Calculadora de Aguinaldo</h1>
-        <div class="row">
-            <div class="col-xs-12">
-                <hr>
-                <h2 class="Left">Salario bruto de los últimos meses</h2>
-                <br><br>
-                <ul>
-                    <li>En cada una de las siguientes casillas se debe digitar el salario bruto devengado en cada mes según corresponda (Salario Bruto: Salario que incluye horas extras, comisiones, bonificaciones, sin rebajas de cargas sociales ni renta). <br><br></li>
-                </ul>
-                <hr>
-            </div>
-            <form action="../Controlador/CalculoAguinaldo.php" method="POST">
-                <div class="col-lg-6 col-xs-12">
-                    <div class="form-group">
-                        <label for="diciembre">Diciembre (2023):</label>
-                        <input name="salarios[]" type="text" id="diciembre" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="enero">Enero:</label>
-                        <input name="salarios[]" type="text" id="enero" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="febrero">Febrero:</label>
-                        <input name="salarios[]" type="text" id="febrero" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="marzo">Marzo:</label>
-                        <input name="salarios[]" type="text" id="marzo" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="abril">Abril:</label>
-                        <input name="salarios[]" type="text" id="abril" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="mayo">Mayo:</label>
-                        <input name="salarios[]" type="text" id="mayo" class="form-control numeroConFormato">
-                    </div>
-                </div>
-                <div class="col-lg-6 col-xs-12">
-                    <div class="form-group">
-                        <label for="junio">Junio:</label>
-                        <input name="salarios[]" type="text" id="junio" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="julio">Julio:</label>
-                        <input name="salarios[]" type="text" id="julio" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="agosto">Agosto:</label>
-                        <input name="salarios[]" type="text" id="agosto" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="setiembre">Setiembre:</label>
-                        <input name="salarios[]" type="text" id="setiembre" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="octubre">Octubre:</label>
-                        <input name="salarios[]" type="text" id="octubre" class="form-control numeroConFormato">
-                    </div>
-                    <div class="form-group">
-                        <label for="noviembre">Noviembre:</label>
-                        <input name="salarios[]" type="text" id="noviembre" class="form-control numeroConFormato">
-                    </div>
-                </div>
-                <div class="col-xs-12 SalarioEspecieContainer">
-                    <hr>
-                    <div class="Left">
-                        <h2>Salario en especie</h2>
-                        (Si aplica)
-                    </div>
-                    <div class="Left">
-                        <div class="form-group">
-                            <input type="radio" name="salarioEspecie" value="0" class="salarioEspecioRadio" checked="checked">
-                            Sin salario en especie<br>
-                        </div>
-                        <div class="form-group">
-                            <input type="radio" name="salarioEspecie" value="1" class="salarioEspecioRadio">
-                            Porcentaje de salario en especie<br>
-                            <input name="esPorcentaje" type="hidden" value="1">
-                            <input name="salarioEspecieValor" type="number" id="PorcentajeEspecieTextBox" class="form-control" style="display: none;">
-                        </div>
-                        <div class="form-group">
-                            <input type="radio" name="salarioEspecie" value="2" class="salarioEspecioRadio">
-                            Monto mensual en especie<br>
-                            <input name="montoMensual" type="text" id="MontoMensualEspecieTextBox" class="form-control numeroConFormato" style="display: none;">
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </div>
-                <!-- Campo oculto para el ID del empleado -->
-                <input type="hidden" name="Empleados_Persona_Cedula" value="<?php echo htmlspecialchars($_SESSION['user']['Persona_Cedula']); ?>">
-                <input type="submit" value="Calcular" class="btn btn-primary marginCenter">
-                <hr>
-            </form>
+    <br>
+    <h1>Calculadora de Aguinaldo</h1>
+    <div class="row">
+        <div class="col-xs-12">
+            <hr>
+            <h2 class="Left">Salario bruto de los últimos meses</h2>
+            <br><br>
+            <ul>
+                <li>En cada una de las siguientes casillas se debe digitar el salario bruto devengado en cada mes según corresponda (Salario Bruto: Salario que incluye horas extras, comisiones, bonificaciones, sin rebajas de cargas sociales ni renta). <br><br></li>
+            </ul>
+            <hr>
         </div>
+        <form action="../Controlador/CalculoAguinaldo.php" method="POST">
+            <div class="col-lg-6 col-xs-12">
+                <div class="form-group">
+                    <label for="diciembre">Diciembre (2023):</label>
+                    <input name="salarios[]" type="text" id="diciembre" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="enero">Enero:</label>
+                    <input name="salarios[]" type="text" id="enero" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="febrero">Febrero:</label>
+                    <input name="salarios[]" type="text" id="febrero" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="marzo">Marzo:</label>
+                    <input name="salarios[]" type="text" id="marzo" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="abril">Abril:</label>
+                    <input name="salarios[]" type="text" id="abril" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="mayo">Mayo:</label>
+                    <input name="salarios[]" type="text" id="mayo" class="form-control numeroConFormato">
+                </div>
+            </div>
+            <div class="col-lg-6 col-xs-12">
+                <div class="form-group">
+                    <label for="junio">Junio:</label>
+                    <input name="salarios[]" type="text" id="junio" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="julio">Julio:</label>
+                    <input name="salarios[]" type="text" id="julio" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="agosto">Agosto:</label>
+                    <input name="salarios[]" type="text" id="agosto" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="setiembre">Setiembre:</label>
+                    <input name="salarios[]" type="text" id="setiembre" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="octubre">Octubre:</label>
+                    <input name="salarios[]" type="text" id="octubre" class="form-control numeroConFormato">
+                </div>
+                <div class="form-group">
+                    <label for="noviembre">Noviembre:</label>
+                    <input name="salarios[]" type="text" id="noviembre" class="form-control numeroConFormato">
+                </div>
+            </div>
+            <div class="col-xs-12 SalarioEspecieContainer">
+                <hr>
+                <div class="Left">
+                    <h2>Salario en especie</h2>
+                    (Si aplica)
+                </div>
+                <div class="Left">
+                    <div class="form-group">
+                        <input type="radio" name="salarioEspecie" value="0" class="salarioEspecioRadio" checked="checked">
+                        Sin salario en especie<br>
+                    </div>
+                    <div class="form-group">
+                        <input type="radio" name="salarioEspecie" value="1" class="salarioEspecioRadio">
+                        Porcentaje de salario en especie<br>
+                        <input name="esPorcentaje" type="hidden" value="1">
+                        <input name="salarioEspecieValor" type="number" id="PorcentajeEspecieTextBox" class="form-control" style="display: none;">
+                    </div>
+                    <div class="form-group">
+                        <input type="radio" name="salarioEspecie" value="2" class="salarioEspecioRadio">
+                        Monto mensual en especie<br>
+                        <input name="montoMensual" type="text" id="MontoMensualEspecieTextBox" class="form-control numeroConFormato" style="display: none;">
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!-- Campo oculto para el ID del empleado -->
+            <input type="hidden" name="Empleados_Persona_Cedula" value="<?php echo htmlspecialchars($_SESSION['user']['Persona_Cedula']); ?>">
+            <input type="submit" value="Calcular" class="btn btn-primary marginCenter">
+            <hr>
+        </form>
     </div>
+</div>
 
     <footer class="footer-Calc-Extras">
         <?php include 'Footer.php'; ?>
