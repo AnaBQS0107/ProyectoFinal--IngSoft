fd2aa8001896c4eb0fa6e91ebdaaab4d21ee0626
Maria <mabreca15@gmail.com>
2024-07-06 22:36:23 -0600

    arreglos de estilos

--- Vista/ReporteCobroMes.php
+++ Vista/ReporteCobroMes.php
@@ -13,7 +13,7 @@ if (isset($resultados) && (is_array($resultados) || is_object($resultados))) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exportar a PDF</title>
     <link rel="icon" type="image/png" href="../img/icono.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Estilos/ReporteCobroMes.css">
 </head>
 <body>
 <header>
@@ -49,7 +49,6 @@ if (isset($resultados) && (is_array($resultados) || is_object($resultados))) {
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 <footer>
     <?php include 'Footer.php'; ?>
