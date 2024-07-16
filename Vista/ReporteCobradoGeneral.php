a9fe691e45cd3a33ff4aefc09b279caccca1155d
Maria <mabreca15@gmail.com>
2024-07-16 14:24:50 -0600

    arreglos css

--- Vista/ReporteCobradoGeneral.php
+++ Vista/ReporteCobradoGeneral.php
@@ -11,12 +11,13 @@ if ($resultadoTotal > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reporte - Monto Total Recaudado por Puestos</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Estilos/ReporteCobradoGeneral.css">
     <link rel="icon" type="image/png" href="../img/icono.png">
 </head>
 <header>
     <?php include 'Header.php'; ?>
 </header>
+<br><br><br><br>
 <body>
 <div class="container mt-5">
     <br><br>
@@ -25,22 +26,23 @@ if ($resultadoTotal > 0) {
     <div class="alert alert-info" role="alert">
         Monto total recaudado: $ <?php echo number_format($resultadoTotal, 2); ?>
     </div>
-
+<br><br>
     <center><form action="../Reportes/pdfCobroGeneral.php" method="post" target="_blank">
         <input type="hidden" name="tipo_reporte" value="monto_total_cinco_puestos">
         <button type="submit" class="btn btn-primary">Exportar a PDF</button>
     </form></center>
-    <br><br><br>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-</body>
+
+
 <footer>
     <?php include 'Footer.php'; ?>
     </footer>
+
+</body>
 </html>
 
 <?php
